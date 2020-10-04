21b5c5d6cc16cfdd81859fa2da6d6de468c5cd19
Piotr Proszowski <p.proszowski@gmail.com>
2020-10-04 10:17:01 +0200

    Initialize repo

---
+++ src/main/java/com/proszkie/githubissueshunter/GithubIssuesHunterApplication.java
@@ -0,0 +1,13 @@
+package com.proszkie.githubissueshunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIssuesHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubIssuesHunterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proszkie/githubissueshunter/GithubIssuesHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proszkie.githubissueshunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIssuesHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
